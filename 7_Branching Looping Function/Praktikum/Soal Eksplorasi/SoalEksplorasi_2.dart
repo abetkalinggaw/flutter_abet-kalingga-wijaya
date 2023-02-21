924ea3215b0d4b1105ace62be1a4c7ea63dfe192
Abet Kalingga Wijaya <75027682+abetkalinggaw@users.noreply.github.com>
2023-02-21 18:24:19 +0700

    Update code SoalEksplorasi_2.dart

--- 7_Branching Looping Function/Praktikum/Soal Eksplorasi/SoalEksplorasi_2.dart
+++ 7_Branching Looping Function/Praktikum/Soal Eksplorasi/SoalEksplorasi_2.dart
@@ -3,14 +3,14 @@ import 'dart:io';
 
 main() {
   print("======= PROGRAM TABEL PERKALIAN =======");
-  int i, j;
+  var col, row;
   stdout.write("Masukan angka : ");
   int angka = int.parse(stdin.readLineSync()!);
   print("--" * 20);
 
-  for (i = 1; i <= angka; i++) {
-    for (j = 1; j <= angka; j++) {
-      stdout.write(i * j);
+  for (col = 1; col <= angka; col++) {
+    for (row = 1; row <= angka; row++) {
+      stdout.write(col * row);
       stdout.write("\t");
     }
     print(" ");
